83beea88e082fc11bf11b16b408d0636191f6d9e
sz <1911392294@qq.com>
2023-02-13 14:07:44 +0800

    将excel转为json

---
+++ 将excel转为json/excelToJson.py
@@ -0,0 +1,26 @@
+import genanki
+import xlrd
+self_array = []
+import json
+
+def read_xlsx():
+    workbook = xlrd.open_workbook(r"C:\Users\geniusShi\Desktop\01.Excel版本1999-2009分句\1999\Text1.xlsx")
+    Words = workbook.sheet_by_name("Sheet1")
+    rows = Words.nrows
+    word_list = []
+
+    # 便利了excel中 第一行 所有的单词
+    for x in range(0, rows):
+        self_json = {}
+        english = Words.row(x)[0].value
+        self_json["english"] = Words.row(x)[0].value
+        self_json["translate"] = Words.row(x)[1].value
+        self_array.append(self_json)
+
+
+
+if __name__ == '__main__':
+    read_xlsx()
+    filename = 'pi_x.json'
+    with open (filename,'w') as f:
+      json.dump(self_array,f)
