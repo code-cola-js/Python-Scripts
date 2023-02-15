a3f0ac7ea63639b4209cd1ab7ded315373fb7d67
sz <1911392294@qq.com>
2023-02-15 10:41:29 +0800

    可以导入微信小程序的json

---
+++ 可以导入微信小程序的json/index.py
@@ -0,0 +1,22 @@
+import json
+import glob
+
+# 所有要合并的json文件所在的文件夹路径
+folder_path = r"C:\Users\geniusShi\Desktop\01.Excel版本1999-2009分句\1999"
+
+# 读取所有json文件
+json_files = glob.glob(folder_path + '/*.json')
+
+# 用于存储所有JSON数据的列表
+all_data = []
+
+# 循环读取每个JSON文件
+for file in json_files:
+    print(file)
+    with open(file, 'r') as f:
+        data = json.load(f)
+        # all_data.append(data)
+        with open(folder_path + '\\merge' +'.json', 'a') as f:
+            json.dump(data, f)
+
+# 将所有JSON数据写入新文件
