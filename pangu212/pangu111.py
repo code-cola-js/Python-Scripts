1e27ea0b29094a31a8101d4e4ebcf7ecbe833808
shizheng <ZhengShiWork@outlook.com>
2023-05-26 11:55:26 +0800

    提交

---
+++ pangu212/pangu111.py
@@ -0,0 +1,8 @@
+import pangu
+
+new_text = pangu.spacing_text('當你凝視著bug，bug也凝視著你')
+# new_text = '當你凝視著 bug，bug 也凝視著你'
+
+# nwe_content = pangu.spacing_file('path/to/file.txt')
+# nwe_content = '與 PM 戰鬥的人，應當小心自己不要成為 PM'
+print(new_text)
